a3be53e8e0420fcbfb74a1ae67b955628fb7adba
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-23 10:42:43 -0800

    built scaffold and basic contents of Startup, Index, Results, TimePerson, HomeController, StyleSheet

---
+++ TimePersonOfTheYear/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimePersonOfTheYear.Controllers;
+
+namespace TimePersonOfTheYear.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Index(int startYear, int endYear)
+        {
+            List<string> winners = new List<string> { "", "" };
+
+            return RedirectToAction("Results", winners);
+        }
+
+        [HttpGet]
+        public IActionResult Result(List<string> winners)
+        {
+            return View();
+        }
+
+        //// model binding example
+        //public string Balloon(int numberOfBalloons)
+        //{
+        //    return $"You have {numberOfBalloons} balloons";
+        //}
+    }
+}
---
+++ TimePersonOfTheYear/Models/TimePerson.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimePersonOfTheYear.Models
+{
+    public class TimePerson
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+
+        public static List<TimePerson> GetPersons(int startYear, int endYear)
+        {
+            TimePerson larry = new TimePerson();
+            TimePerson curly = new TimePerson();
+            TimePerson moe = new TimePerson();
+            List<TimePerson> winners = new List<TimePerson>() { larry, curly, moe };
+
+            // TODO: query data source, build objects, populate list
+
+            return winners;
+        }
+    }
+}
